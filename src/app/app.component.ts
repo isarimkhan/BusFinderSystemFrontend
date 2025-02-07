0f95ad2f0bdcce2e5908a400288d6ea5f3de69e7
Muhammad Sarim Kham <136439929+isarimkhan@users.noreply.github.com>
2025-02-08 00:32:02 +0500

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./navbar/navbar.component";
-import { FooterComponent } from "./footer/footer.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
