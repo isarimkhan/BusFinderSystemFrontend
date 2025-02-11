eaecab14633c0563c9c9f4cceb5bdf20dc903f13
Muhammad Sarim Kham <136439929+isarimkhan@users.noreply.github.com>
2025-02-12 00:26:49 +0500

    ww

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { FooterComponent } from './component/footer/footer.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
-=======
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { FooterComponent } from "./components/footer/footer.component";
->>>>>>> 33323e8a4aaaf959df12a65bebd62071997ce0e8
+
 
 @Component({
   selector: 'app-root',
