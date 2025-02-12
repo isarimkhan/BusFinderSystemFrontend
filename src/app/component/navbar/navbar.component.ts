a2a025bf1564c0c45dfb22c64821be09297e5318
Muhammad Sarim Kham <136439929+isarimkhan@users.noreply.github.com>
2025-02-13 02:32:03 +0500

    nav half

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
+interface DropdownState {
+  Plan: boolean;
+  Book: boolean;
+  Card: boolean;
+  Fleet: boolean;
+  login:boolean;
+  vehicle:boolean;
+  renter:boolean;
+}
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isMenuOpen = false;
+  dropdowns: DropdownState = {
+    Plan: false,
+    Book: false,
+    Card: false,
+    Fleet:false,
+    login:false,
+    vehicle:false,
+    renter:false
+  };
 
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (!(event.target as HTMLElement).closest('.nav-item')) {
+      this.closeAllDropdowns();
+    }
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleDropdown(menu: keyof DropdownState, event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.closeAllDropdowns();
+    this.dropdowns[menu] = !this.dropdowns[menu];
+  }
+
+  closeAllDropdowns() {
+    Object.keys(this.dropdowns).forEach(key => {
+      this.dropdowns[key as keyof DropdownState] = false;
+    });
+  }
 }
